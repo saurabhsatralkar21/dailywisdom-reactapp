0def04bc18631cf16a56b5ab7129faf44fb21b44
Saurabh S <saurabh.satralkar7@gmail.com>
2023-01-27 23:01:31 +0100

    Spinner added

--- src/components/Wisdom.js
+++ src/components/Wisdom.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import CircleLoader from "react-spinners/ClipLoader";
 
 const Wisdom = () => {
 
     const [advise, setAdvise] = useState("");
+    const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
+        setLoading(true);
         const adviseEndpoint = "https://api.adviceslip.com/advice";
         
         const fetchData = async () => {
@@ -18,15 +21,24 @@ const Wisdom = () => {
             console.log(error);
         }
         }
-        fetchData();
 
+        setTimeout(()=>{
+            fetchData();
+            setLoading(false);
+        }, 3000);
+    
     },[])
 
     return(
         <div>
             <h1 className="heading"> Wisdom you take, young padawan! </h1>
             <div className='adviseBox'>
-                <p>{advise}</p>
+                { loading ?
+                
+            <CircleLoader color={'#301E67'} loading={loading} size={100} aria-label="Loading Spinner" data-testid="loader" />
+                : <p>{advise}</p>
+                }
+                
               </div>
         </div>
     )
